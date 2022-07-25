610568d13c0bb87b4969044b8c868b7ee5242566
devops <diana.behshad@gmail.com>
2022-07-25 14:02:13 +0430

    Authentication with jwtToken

--- src/core/auth/index.js
+++ src/core/auth/index.js
@@ -5,8 +5,10 @@ class AuthenticationManager{
     static getJwtToken(payload){
         const token = jwt.sign(payload, jwt_config.secret, {
             algorithm: 'HS256',
-            expiresIn: jwt_config.expirySecond * 1000
+            expiresIn: Number(jwt_config.expirySecond) 
         });
         return token;
     }
 }
+
+module.exports = AuthenticationManager;
--- src/core/middleware/auth.js
+++ src/core/middleware/auth.js
@@ -1,18 +1,22 @@
 const UserReader = require("../../module/users/model/read");
+const AuthenticationManager = require("../auth");
 
 class authMiddleware {
   static async login(req, res, next) {
     try {
       const { username, password } = req.body;
-      const user = await UserReader.getUserByUsernameAndPassword(
-        username,
-        password
-      );
-      const payload = {
-        id: user.id,
-        username: user.username,
-      };
-      res.send(user);
+      const user = await UserReader.getUserByUsernameAndPassword(username, password);
+      if (!user) {
+        res.status(401).end();
+      } else {
+        const payload = {
+          id: user.id,
+          username: user.username,
+        };
+
+        const jwtToken = AuthenticationManager.getJwtToken(payload);
+        res.send(jwtToken);
+      }
     } catch (error) {
       res.status(500).send(error.message);
     }
