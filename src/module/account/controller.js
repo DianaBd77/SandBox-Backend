9df7638fc1c82b6c17bdd55d45cf5e4f1e41494c
devops <diana.behshad@gmail.com>
2022-08-03 19:08:10 +0430

    Get token after sign-up

--- src/module/account/controller.js
+++ src/module/account/controller.js
@@ -1,19 +1,26 @@
 const UserCreator = require("./model/create");
 const DatabaseManager = require("../../core/database/databaseManager");
+const AuthenticationManager = require("../../core/auth");
 
 class UserController {
   static async createNewUser(req, res, next) {
     try {
       const userData = req.body;
-      const { email } = userData;
+      const { email, username } = userData;
       const query = `SELECT * FROM user WHERE email = '${email}'`;
       const findEmail = await DatabaseManager.query(query);
-      console.log('findEmail :>> ', findEmail[0]);
       if (findEmail[0][0]) {
         res.status(409).end("Email Already Exist!");
       } else {
         const result = await UserCreator.createUser(userData);
-        res.send(result);
+        const payload = {
+          id: email,
+          username: username,
+        };
+
+        const jwt = await AuthenticationManager.getJwtToken(payload);
+        res.json(jwt);
+        // res.send(result);
       }
     } catch (error) {
       next(error);
