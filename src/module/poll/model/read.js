74f3ba2b1d95df7476549b1d816bde5e8ba1b97b
devops <diana.behshad@gmail.com>
2022-08-10 14:00:44 +0430

    Get poll

--- src/module/poll/model/read.js
+++ src/module/poll/model/read.js
@@ -1,38 +1,26 @@
 const DatabaseManager = require("../../../core/database/databaseManager");
 
+
 class PollReader {
   static async getAllPolls(userID) {
     const query = `
-      SELECT poll.*, COUNT(p.id) as participants, p.name
-      FROM poll LEFT JOIN participant p on poll.id = p.poll_id
-      WHERE poll.user_id = ${userID}
-      GROUP BY p.poll_id;
+      SELECT title, description, link, img_url
+      FROM poll 
+      WHERE user_id = ${userID};
     `;
     const result = await DatabaseManager.query(query);
     return result[0];
   }
 
-  static async getPollByID(userID, id) {
-    //2 queries with less inner joins or?!
+  static async getPollByID(userID, uuid) {
+
     const query = `
-      SELECT p.name, o.option, p2.*
-      FROM participant p
-              INNER JOIN choice c on p.id = c.participant_id
-              INNER JOIN options o on c.option_id = o.id
-              INNER JOIN poll p2 on o.poll_id = p2.id
-      WHERE p2.id = ${id}
-              AND p2.user_id = ${userID};
+          SELECT title, description, link, img_url
+          FROM poll
+          WHERE link = '${uuid}'
+                And user_id = ${userID};
     `;
 
-    // SELECT p.name, o.`option`
-    // FROM participant p
-    //          INNER JOIN choice c on p.id = c.participant_id
-    //          INNER JOIN options o on c.option_id = o.id
-    // WHERE o.poll_id = 1;
-
-    // SELECT *
-    // FROM poll
-    // WHERE id = 1;
     const result = await DatabaseManager.query(query);
     return result[0];
   }
