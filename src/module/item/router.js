72758b97a798a174bbebe2cfb75421452c01063a
devops <diana.behshad@gmail.com>
2022-08-11 15:31:55 +0430

    Add item creator validation

--- src/module/item/router.js
+++ src/module/item/router.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 
 router.get('/:id', ItemValidator.getItemsByPollIDSchema , AuthMiddleware.jwtTokenValidation, ItemController.getAllItems);
-router.post('/', AuthMiddleware.jwtTokenValidation, ItemController.createNewItem);
+router.post('/', ItemValidator.createItemSchema ,AuthMiddleware.jwtTokenValidation, ItemController.createNewItem);
 router.delete('/:id', ItemValidator.deleteItemSchema ,AuthMiddleware.jwtTokenValidation, ItemController.removeItem);
 
 
