ef9a7b5794baa7d175b75af0cabbbe8a3c9c4ed7
devops <diana.behshad@gmail.com>
2022-08-11 15:29:45 +0430

    Change item to name in database

--- src/module/item/model/read.js
+++ src/module/item/model/read.js
@@ -4,8 +4,8 @@ const DatabaseManager = require("../../../core/database/databaseManager");
 class ItemReader {
   static async getAllItems(id) {
     const query = `
-      SELECT item
-      FROM items
+      SELECT name
+      FROM item
       WHERE poll_id = ${id};
     `;
     const result = await DatabaseManager.query(query);
