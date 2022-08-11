719fd5027cbfc01839f0d3ee5ff1df24fa4c29f1
devops <diana.behshad@gmail.com>
2022-08-11 15:30:02 +0430

    Delete items by poll ID

---
+++ src/module/item/model/delete.js
@@ -0,0 +1,13 @@
+const DatabaseManager = require("../../../core/database/databaseManager");
+
+class ItemRemover {
+  static async deleteItemByID(id) {
+    const query = `
+    DELETE FROM item
+    WHERE poll_id = ${id}`;
+    const result = await DatabaseManager.query(query);
+    return result;
+  }
+}
+
+module.exports = ItemRemover;
