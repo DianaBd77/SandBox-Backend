e1eee0d398d75107d6173160edfaffc777957977
devops <diana.behshad@gmail.com>
2022-08-11 15:28:39 +0430

    Edit item validation

--- src/module/item/validation/index.js
+++ src/module/item/validation/index.js
@@ -3,21 +3,22 @@ const { celebrate, Joi, Segments } = require("celebrate");
 class PollValidator {
   static getItemsByPollIDSchema = celebrate({
     [Segments.PARAMS]: Joi.object().keys({
-        id: Joi.number().integer().positive().required(),
+      id: Joi.number().integer().positive().required(),
     }),
   });
 
-//   static createItemSchema = celebrate({
-//     [Segments.BODY]: Joi.object().keys({
-//       poll_id: Joi.number().integer().positive().required(),
-//       item: Joi.string().required(),
-//     }),
-//   });
-
+  static createItemSchema = celebrate({
+    [Segments.BODY]: Joi.array().items(
+      Joi.object().keys({
+        poll_id: Joi.number().integer().positive().required(),
+        item: Joi.string().required(),
+      })
+    ).required().min(1),
+  });
 
   static deleteItemSchema = celebrate({
     [Segments.PARAMS]: Joi.object().keys({
-        id: Joi.number().integer().positive().required(),
+      id: Joi.number().integer().positive().required(),
     }),
   });
 }
