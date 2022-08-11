5ef872170eb06624c37b348cf86c48247d312730
devops <diana.behshad@gmail.com>
2022-08-11 22:09:54 +0430

    Get all participants name with poll ID

---
+++ src/module/participant/model/reade.js
@@ -0,0 +1,17 @@
+const DatabaseManager = require("../../../core/database/databaseManager");
+
+
+class ParticipantReader {
+  static async getAllParticipant(id) {
+    const query = `
+      SELECT name
+      FROM participant
+      WHERE poll_id = ${id};
+    `;
+    const result = await DatabaseManager.query(query);
+    return result[0];
+  }
+
+}
+
+module.exports = ParticipantReader;
